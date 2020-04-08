481ce52ccd7a3f7959739392c0d84e80ce04a67b
Silvano Gomes do Amaral <silvano.amaral@spcdc.br>
2020-04-08 18:02:07 -0300

    Aplicando grafico line

---
+++ src/components/LineChart/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Line } from 'react-chartjs-2'
+
+const LineChart = props => {
+  const {
+    data,
+    refs,
+    options
+  } = props
+
+  return <div className="chart__line">
+    <Line
+      ref={refs}
+      data={data}
+      options={options}
+      height={250}
+    />
+  </div>
+}
+
+export default LineChart
--- src/view/Page2/index.js
+++ src/view/Page2/index.js
@@ -1,7 +1,95 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import axios from 'axios'
+
+import './Page2.scss'
+
+import LineChart from '../../components/LineChart'
 
 const Page2 = () => {
-  return 'pagina 2'
+  const [data, setData] = useState(false)
+
+  const chartLine = useRef()
+
+  const options = {
+    responsive: true,
+    maintainAspectRatio: false,
+    legend: {
+      align: 'end',
+      labels: {
+        boxWidth: 5,
+        usePointStyle: true,
+        strokeStyle: Color,
+        padding: 25
+      }
+    },
+    scales: {
+      xAxes: [
+        {
+          gridLines: {
+            display: true,
+            borderDash: [3, 3]
+          }
+        }
+      ],
+      yAxes: [
+        {
+          gridLines: {
+            display: true,
+            borderDash: [3, 3]
+          }
+        }
+      ]
+    }
+  }
+
+  useEffect(() => {
+    const resultAnual = axios('api/result', { params: { path: 'time-data' } })
+
+    resultAnual.then(res => {
+      const dataToday = res.data.today.map(item => item.value)
+      const dataYesterday = res.data.yesterday.map(item => item.value)
+      const label = res.data.today.map(item => item.label)
+      
+      const data = {
+        labels: label,
+        datasets: [
+          {
+            label: "Today",
+            fill: false,
+            borderCapStyle: 'butt',
+            pointBackgroundColor: '#fff',
+            lineTension: 0.1,
+            borderColor: '#303F9F',
+            borderWidth: 2,
+            borderDash: [0, 0],
+            data: dataToday
+          },
+          {
+            label: "Yesterday",
+            fill: false,
+            pointBackgroundColor: '#fff',
+            lineTension: 0.1,
+            borderColor: '#BF3FFF',
+            borderWidth: 2,
+            data: dataYesterday
+          }
+        ]
+      }
+      setData(data)
+    })
+  },[])
+
+  return <div className="content">
+  <h2>Pagina 2</h2>
+  <div className="chart chart__line">
+    <div className="row">
+      <strong>Line Chart 2 Data</strong>
+      {data &&
+        <LineChart refs={chartLine} data={data} options={options} />
+      }
+    </div>
+  </div>
+</div>
 }
 
 export default Page2
