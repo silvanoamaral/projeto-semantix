3c2bc7f3316ee3f89a9753c7628e031dde84df56
Silvano Gomes do Amaral <silvano.amaral@spcdc.br>
2020-04-09 11:34:32 -0300

    adicionando rodape e header

---
+++ src/common/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Header.scss'
+
+const Header = () => {
+  return <header>
+    <div className="container">
+      <img class="img-fluid logo" src="https://semantix.com.br/wp-content/uploads/2019/12/Semantix-horizontal.png" alt="xamin" />
+    </div>
+  </header>
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 
 import App from './view/App'
+import Footer from './common/Footer'
+import Header from './common/Header'
 import History from './helpers/history'
 
 ReactDOM.render(
   <BrowserRouter>
+    <Header />
     <App />
+    <Footer />
   </BrowserRouter>,
 document.getElementById('root'))
