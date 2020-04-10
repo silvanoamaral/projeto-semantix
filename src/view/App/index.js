53125351969014fc74c25012b1060cf2e920b2eb
Silvano Gomes do Amaral <silvano.amaral@spcdc.br>
2020-04-10 14:25:29 -0300

    refatorando component APP

--- src/view/App/index.js
+++ src/view/App/index.js
@@ -2,16 +2,20 @@ import React from 'react'
 
 import MenuLateral from '../../common/MenuLateral'
 import Routes from '../../routes'
+import Footer from '../../common/Footer'
+import Header from '../../common/Header'
 
 import '../../assets/css/index.scss'
 
 const App = () => {
-  return (
+  return (<>
+    <Header />
     <div className="container">
       <MenuLateral />
       <Routes />
     </div>
-  )
+    <Footer />
+  </>)
 }
 
 export default App
